8172459ec2fbe235542592ca029fb3cca5cb2c40
Ilker Eker <lkrkr@hotmail.com>
2022-10-07 16:45:56 +0300

    Add project files.

---
+++ MovieAPI/Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿namespace MovieAPI.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+
+        public string Name { get; set; }
+
+
+        public string Genre { get; set; }
+
+
+        public string Duration { get; set; }
+
+
+        public DateTime ReleaseDate { get; set; }
+
+
+    }
+
+}
---
+++ MovieAPI/Models/MoviesDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MovieAPI.Models
+{
+    public class MoviesDbContext:DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+        public MoviesDbContext(DbContextOptions<MoviesDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().ToTable("Movie");
+        }
+    }
+}
